14e5f0932a12b3da7d33851acb0765272cb00911
Gabriel Bernardi <115042820+GoldenLeap@users.noreply.github.com>
2025-05-29 16:37:27 -0300

    Adiciona localstorage ao sistema de login

--- scripts/login.js
+++ scripts/login.js
@@ -1,33 +1,67 @@
-function exibirMensagem(texto, tipo){
-    const mensagem = document.getElementById("mensagem")
-    mensagem.textContent = texto
-    
-    mensagem.className = `mensagem ${tipo}`
-    mensagem.classList.remove("hidden")
+function exibirMensagem(texto, tipo) {
+  const mensagem = document.getElementById("mensagem");
+  mensagem.textContent = texto;
 
-    setTimeout(()=>{
-        mensagem.classList.add("hidden")
-    }, 2500)
+  mensagem.className = `mensagem ${tipo}`;
+  mensagem.classList.remove("hidden");
+
+  setTimeout(() => {
+    mensagem.classList.add("hidden");
+  }, 2500);
 }
 
+function mostrarSenha(id) {
+  var input = document.getElementById(id);
+  var tipo = input.type;
 
-function validarLogin(){
-    const usuario = document.getElementById("usuario").value
-    const senha = document.getElementById("senha").value
+  if (tipo === "password") {
+    input.type = "text";
+  } else {
+    input.type = "password";
+  }
+}
 
-   /* Usuario fixo para exemplo */
-   const usuarioTeste = "usuarioLegal"
-   const senhaTeste = "senhaLegal"
-   
-   if(usuario === usuarioTeste && senha === senhaTeste){
-    exibirMensagem("Login realizado", "sucesso")
-    setTimeout(()=>{
-        
-        window.location.href = "/index.html"
-    }, 1000)
-    
-   }else{
-    exibirMensagem("Usuario/email ou senha incorretos", "erro")
-   }
+function validarLogin() {
+  const usuario = document.getElementById("usuario").value.trim();
+  const senha = document.getElementById("senha").value.trim();
+  const usuariosRegistrados =
+    JSON.parse(localStorage.getItem("usuarios")) || [];
 
+  // Tenta encontrar o usuário no localStorage
+  const uObj = usuariosRegistrados.find((u) => u.usuario === usuario);
+
+  // Caso usuário seja encontrado e senha correta
+  if (uObj && uObj.senha === senha) {
+    exibirMensagem("Login realizado com sucesso", "sucesso");
+    setTimeout(() => {
+      window.location.href = "./index.html"; // ou outro destino
+    }, 1000);
+    return; // impede execução do restante
+  }
+
+  // Usuário e senha de teste 
+  const usuarioTeste = "usuarioLegal";
+  const senhaTeste = "senhaLegal";
+  
+  // Usuario e senha adm(para testes)
+  const usuarioAdm = "adm"
+  const senhaAdm = "adm123"
+
+  if (usuario === usuarioTeste && senha === senhaTeste) {
+    exibirMensagem("Login realizado com sucesso", "sucesso");
+    setTimeout(() => {
+      window.location.href = "./index.html";
+    }, 1000);
+    return;
+  }
+  if(usuario === usuarioAdm && senha === senhaAdm){
+    exibirMensagem("Login realizado com sucesso", "sucesso")
+    setTimeout(() => {
+        window.location.href = "./admin.html";
+    }, 1000);
+    return
+}
+
+  // Se nenhuma das opções funcionou, exibe erro
+  exibirMensagem("Usuário ou senha incorretos", "erro");
 }
